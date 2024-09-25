2c1cda17097a04e8dddbaa7923255b9955c0bd20
Jorash Robillos <jorashjonathan.robillos@cit.edu>
2024-09-25 11:44:59 +0800

    init

---
+++ books/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import BorrowedBookForm
+
+# Create your views here.
+def book_list(request):
+    books = Book.objects.all
+    if request.method == 'POST':
+        form = BorrowedBookForm(request.POST)
+        if form.is_valid():
+            book_id = request.POST.get('book_id')
+            book = Book.objects.get(id=book_id)
+            book.borrowed_by = form.cleaned_data['borrowed_by']
+            book.due_date = form.cleaned_data['due_date']
+            book.save()
+
+            return redirect('book_list')
+    else:
+        form = BorrowedBookForm
+    return render(request, 'books/book_list.html', {'books': books, 'form': form})
